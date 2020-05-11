0032a43cac10fc30f06f5e9c4ace4d0804d88a87
vishal <vishal.ubale@coxautoinc.com>
2020-05-11 16:46:03 +0530

    Add

---
+++ src/HomePage/Base/BasePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class HomePage extends React.Component {
+    componentDidMount() {
+        this.props.getUsers();
+    }
+
+    handleDeleteUser(id) {
+        return (e) => this.props.deleteUser(id);
+    }
+
+    render() {
+        const { user, users } = this.props;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h1>This is base page</h1>
+            </div>
+        );
+    }
+}
